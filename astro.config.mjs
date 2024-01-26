3fc42b9ad34105f50a6ae1db668ef56ee5ecd593
KevinAriasdevWeb <kev.arias.araya@gmail.com>
2024-01-26 02:12:42 -0300

    ruta base 1.5

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,7 +17,7 @@ const SERVER_PORT = 3000;
 // the url to access your blog during local development
 const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
 // the url to access your blog after deploying it somewhere (Eg. Netlify)
-const LIVE_URL = "https://KevinAriasdevWeb.github.io";
+const LIVE_URL = "https://KevinAriasdevWeb.github.io/";
 // this is the astro command your npm script runs
 const SCRIPT = process.env.npm_lifecycle_script || "";
 const isBuild = SCRIPT.includes("astro build");
@@ -29,7 +29,7 @@ if (isBuild) {
 
 export default defineConfig({
   server: { port: SERVER_PORT },
-  site: BASE_URL,
+  site: "portfolio-dev",
   integrations: [
     sitemap(),
     tailwind({
