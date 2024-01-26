543a88ba584b988841127e9bbe6332cbee62e176
KevinAriasdevWeb <kev.arias.araya@gmail.com>
2024-01-26 01:07:27 -0300

    ruta base

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
+import { writeFile, copy } from 'fs/promises';
+
 
 import tailwind from "@astrojs/tailwind";
 
@@ -6,4 +8,23 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   
+   // ...
+   buildOptions: {
+    baseUrl: '/portfolio-dev/',
+  },
+  // ...
+  hooks: {
+    postBuild: async () => {
+      await Promise.all([
+        // Copia archivos estáticos
+        copy('static', '.astro/public'),
+
+        // Copia el directorio dist a la raíz del proyecto
+        copy('.astro/dist', '.'),
+      ]);
+
+      // Añade un archivo CNAME si es necesario
+      await writeFile('.astro/public/CNAME', 'mi-domino.github.io');
+    },
+  },
 });
