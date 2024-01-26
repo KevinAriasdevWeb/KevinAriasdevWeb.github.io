fcd3d294a719b07e4bb613e1d73ebd4b39466bbe
KevinAriasdevWeb <kev.arias.araya@gmail.com>
2024-01-26 01:45:22 -0300

    ruta base 1.3

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,7 +21,7 @@ const LIVE_URL = "https://KevinAriasdevWeb.github.io";
 // this is the astro command your npm script runs
 const SCRIPT = process.env.npm_lifecycle_script || "";
 const isBuild = SCRIPT.includes("astro build");
-let BASE_URL = "portfolio-dev";
+let BASE_URL = LOCALHOST_URL;
 // When you're building your site in local or in CI, you could just set your URL manually
 if (isBuild) {
   BASE_URL = LIVE_URL;
